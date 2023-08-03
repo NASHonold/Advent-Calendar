9d1ad21f0144aabbb81b27ed30857ec85f6a5e49
Nash Honold <nashonold@gmail.com>
2023-08-02 20:24:58 -0700

    reworked sorting and completed part 2

--- Day5/src/Ship.java
+++ Day5/src/Ship.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Stack;
 
 public class Ship {
@@ -6,16 +8,65 @@ public class Ship {
     private ArrayList<Stack<Character>> stacks;
     private int numberOfStacks;
 
+    // ================== Constructor ==================
     public Ship(ArrayList<String> cargo) {
         this.cargoInput = cargo;
         setNumberOfStacks();
         makeStacks();
-        setupStacks();
+        setupStacks(true);
     }
 
-    private void setupStacks() {
+    // ================== Public Methods ==================
+    /**
+     * Resets cargo stacks to original positions
+     */
+    public void resetShipCargo() {
+        makeStacks();
+        setupStacks(false);
+    }
 
-        for (int i = this.cargoInput.size() - 1; i >= 0; i--) {
+    /**
+     * 
+     * @return String of chars that are all top crate chars from l to r
+     */
+    public String getTopCrates() {
+        StringBuilder top = new StringBuilder();
+        this.stacks.forEach((x) -> top.append(x.peek()));
+        return top.toString();
+    }
+
+    /**
+     * Arranges cargo with crane model designation &
+     * set of instructions
+     * 
+     * @param model        - Integer Crane Model - 9000 or 9001
+     * @param instructions - ArrayList of move instructions
+     */
+    public void crateMover(int model, ArrayList<String> instructions) {
+        if (model == 9000 || model == 9001) {
+            instructions.forEach((x) -> moveCrates(model, x));
+        } else
+            System.out.println("You did not select an available Crate Mover!");
+
+    }
+
+    // ================== Private Methods ==================
+
+    /**
+     * Sets up layout of cargo stacks based on
+     * cargo input string list.
+     * 
+     * @param first Boolean that triggers reversal of cargo input string list. This
+     *              is needed as the cargoList object is reversed on first pass but
+     *              during reset, reversal is not needed.
+     * 
+     */
+    private void setupStacks(boolean first) {
+        if (first) {
+            Collections.reverse(cargoInput);
+        }
+
+        for (int i = 0; i < cargoInput.size(); i++) {
             int charIdx = 1;
             String cargoLine = this.cargoInput.get(i);
             for (int j = 0; j < this.numberOfStacks; j++) {
@@ -29,6 +80,9 @@ public class Ship {
 
     }
 
+    /**
+     * Sets up empty stack objs to receive initial char cargo
+     */
     private void makeStacks() {
         ArrayList<Stack<Character>> stacks = new ArrayList<>();
         for (int i = 0; i < this.numberOfStacks; i++) {
@@ -38,24 +92,23 @@ public class Ship {
         this.stacks = stacks;
     }
 
+    /**
+     * Derives the number of stacks from the length of the first string in
+     * cargoStacks and sets this.numberOfStacks
+     */
     private void setNumberOfStacks() {
         this.numberOfStacks = this.cargoInput.get(0).length() / 4 + 1;
     }
 
-    public String getTopCrates() {
-        StringBuilder top = new StringBuilder();
-
-        this.stacks.forEach((x) -> top.append(x.peek()));
-        return top.toString();
-    }
-
-    public void arrangeCargo(ArrayList<String> instructions) {
-
-        instructions.forEach((x) -> moveCrates(x));
-
-    }
-
-    private void moveCrates(String instructionString) {
+    /**
+     * Helper function responsible for sorting/arranging cargo using a set of
+     * instruction in the form of ArrayList<String> obj. Uses model number to
+     * determine appropriate sorting algorithm
+     * 
+     * @param model
+     * @param instructionString
+     */
+    private void moveCrates(int model, String instructionString) {
         String[] instructionSteps = instructionString.split(" ");
         int qtyIdx = 1;
         int originIdx = 3;
@@ -64,11 +117,28 @@ public class Ship {
         int origin = Integer.parseInt(instructionSteps[originIdx]) - 1;
         int dest = Integer.parseInt(instructionSteps[destIdx]) - 1;
 
-        for (int i = 0; i < qty; i++) {
-            char mover = this.stacks.get(origin).pop();
-            this.stacks.get(dest).add(mover);
-        }
+        switch (model) {
+            case 9000:
+                for (int i = 0; i < qty; i++) {
+                    char mover = this.stacks.get(origin).pop();
+                    this.stacks.get(dest).add(mover);
+                }
+                break;
 
+            case 9001:
+                Character[] crates = new Character[qty];
+                for (int i = 0; i < qty; i++) {
+                    Character x = this.stacks.get(origin).pop();
+                    crates[i] = x;
+
+                }
+                Collections.reverse(Arrays.asList(crates));
+                for (int i = 0; i < qty; i++) {
+                    Character x = crates[i];
+                    this.stacks.get(dest).add(x);
+                }
+                break;
+        }
     }
 
 }
--- Day5/src/Solution.java
+++ Day5/src/Solution.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
 
 public class Solution {
 
@@ -20,33 +18,34 @@ public class Solution {
             boolean firstLine = true;
 
             // Read in current cargo layout and instructions in 2 different ArrayList
-            // objects
-            // to be passed to the Ship object later
+            // objects to be passed to the Ship object later
             while ((line = br.readLine()) != null) {
 
                 if (firstLine) {
                     firstLine = false;
                     // create cargo package
-                    while (line.contains("[")) {
+                    while (line.contains("[")) {// all cargo strings have brackets present
                         shipCargo.add(line);
                         line = br.readLine();
                     }
-
-                    System.out.println(br.readLine());
-                } else {
+                    br.readLine();// advance past the string of whitespace
+                } else {// add strings used for commands/instructions to commands package
                     commands.add(line);
                 }
             }
 
-            Ship cargoShip = new Ship(shipCargo);// Create ship and initialize it with current cargo layout
-            cargoShip.arrangeCargo(commands);
             br.close();
+            Ship cargoShip = new Ship(shipCargo);// Create ship and initialize it with current cargo layout
+            cargoShip.crateMover(9000, commands);// use CrateMover 9000 to organize cargo
             System.out.println("=========== Day 5 ===========");
             System.out.println("Part 1 solution: " + cargoShip.getTopCrates());
 
+            cargoShip.resetShipCargo();
+            cargoShip.crateMover(9001, commands); // use CrateMover 9001 to organize cargo
+            System.err.println("Part 2 Solution: " + cargoShip.getTopCrates());
+
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println(e.toString());
         }
 
     }
